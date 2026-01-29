627aa03d5ee764456f1ca3fa430b1f3514efa952
SuhairPasta <suhairrizwanofficial@gmail.com>
2026-01-29 04:46:14 -0800

    9618_s24_41 Question 1 completed by Suhair Pasta

--- 9618_s24_41/Question1_J24.py
+++ 9618_s24_41/Question1_J24.py
@@ -1,13 +1,67 @@
-# a:
+#q1
+#a
+DataStored = [''] * 20 #ARRAY [0:19]
+NumberItems = 0 # integer variable is defined
+#b
+def Initialise():
+    global DataStored
+    global NumberItems
+    NumberItems = int(input("how many numbers would you like to input? "))
+    while NumberItems < 1 or NumberItems > 20:
+        NumberItems = int(input("how many numbers would you like to input? "))
+    for x in range(NumberItems):
+        DataStored[x] = int(input("Write the number to insert in array "))
 
-NumberItems = 20
-DataStored = [] * NumberItems
+#d(i)
+def BubbleSort():
+    global DataStored
+    global NumberItems
+    for t in range(NumberItems):
+        for x in range(NumberItems-1):
+            if DataStored[x] > DataStored[x+1]:
+                temp = DataStored[x]
+                DataStored[x] = DataStored[x+1]
+                DataStored[x+1] = temp
+
+#e(i)
+
+def BinarySearch(DataToFind):
+    global DataStored
+    global NumberItems
+    ub = NumberItems
+    lb = 0
+    midp = int((lb+ub)/2)
+    found = False
+    while not found and lb <= ub:
+
+        if DataStored[midp] == DataToFind:
+            found = True
+        if DataToFind < DataStored[midp]:
+            ub = midp-1
+        else:
+            lb = midp+1
+        midp = int((lb + ub) / 2)
+    if found == True:
+        return midp
+    elif found == False:
+        return -1
 
 
-# b:
-
-def Initialize():
-    global DataStored, NumberItems
-    DataStored = [0] * NumberItems
-
-    Quantity = int(input("Enter the quantity of items (max {}): ".format(NumberItems)))
+#e(ii)
+#c(ii)
+#d(ii)
+def main():
+    global DataStored
+    global NumberItems
+    NumberItems = 0
+    Initialise()
+    for x in range(20):
+        print(DataStored[x] , end=" ")
+    BubbleSort()
+    print()
+    for x in range(20):
+        print(DataStored[x] , end=" ")
+    DataToFind = int(input("Enter the number you would like to find: "))
+    bs = BinarySearch(DataToFind)
+    print(bs)
+main()
